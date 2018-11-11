e9cdf0f34cdc993a39de450d6c4052c64e88525a
Nicholas Chin <31778272+theblackathena@users.noreply.github.com>
2018-11-11 13:27:48 -0800

    Update template.js

--- template.js
+++ template.js
@@ -3,16 +3,16 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "nickjon20",
-  database: 'mydb'
+  password: "",
+  database: 'marketdb'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+    console.log("");
   });
 });
